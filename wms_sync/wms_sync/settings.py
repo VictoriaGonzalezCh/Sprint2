bf34e710405ed7d1d9a3708faed6b92fb5418121
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-09-26 14:04:40 -0500

    host

--- wms_sync/wms_sync/settings.py
+++ wms_sync/wms_sync/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'monitoring_db',
         'USER': 'monitoring_user',
         'PASSWORD': 'isis2503',
-        'HOST': '3.89.186.251',
+        'HOST': '54.226.199.72',
         'PORT': '5432',
     }
 }
