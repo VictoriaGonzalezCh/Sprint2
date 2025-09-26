b5fdeb208ae68051c79e0ed56b875388b595b926
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-09-26 13:05:09 -0500

    urls

--- wms_sync/wms_sync/urls.py
+++ wms_sync/wms_sync/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include  # <- Asegúrate de importar include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sync_app.urls')), 
+    path('', include('sync_app.urls')),  # <- Esto incluye los URLs de tu app
 ]
