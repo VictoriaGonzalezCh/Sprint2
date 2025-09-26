5f0cf4ae0d03b24c1b36dcc9df4f8aa7c18cd2e8
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-09-26 12:18:43 -0500

    urls

--- wms_sync/wms_sync/urls.py
+++ wms_sync/wms_sync/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sync_app.urls')),  
+    path('', include('sync_app.urls')), 
 ]
- 
